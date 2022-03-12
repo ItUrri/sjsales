3680cb884fce2cf5fc73e3df3a2c731fbfff4e35
Iturri <iturri.jon@gmail.com>
2022-03-12 19:24:21 +0100

    todo

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,7 +3,28 @@
 @section('btn-toolbar')
 @endsection
 @section('content')
-  
+
+<form action="" method="GET" class="row mb-3">
+      <label for="colFormLabelSm" class="col-sm-1 col-form-label col-form-label-sm bg-danger">Email</label>
+      <div class="col-sm-2 bg-info">
+        <input type="date" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+      </div>
+      <label for="colFormLabelSm" class="col-sm-1 col-form-label col-form-label-sm bg-danger">Email</label>
+      <div class="col-sm-2 bg-info">
+        <input type="date" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+      </div>
+      <div class="col-sm-2 bg-dark">
+        <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+      </div>
+      <div class="col-sm-2 bg-warning">
+        <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+      </div>
+      <div class="col-sm bg-secondary">
+          {{ Form::button('<span data-feather="search"></span>', ['class' => 'btn btn-sm btn-primary float-end', 'type' => 'sybmit']) }}
+      </div>
+</form>
+
+
 @include('orders.shared.table', ['collection' => $collection, 'pagination' => true])
   
 @endsection
